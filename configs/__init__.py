9a9c5c94a31c2a92b9e4b53b48b1714210514603
hongngoc1210 <ngochnh121@gmail.com>
2025-12-04 23:04:54 +0700

    Fix yaml

--- configs/__init__.py
+++ configs/__init__.py
@@ -2,6 +2,6 @@ import yaml
 import os
 
 # Tự động load file YAML làm config mặc định
-yaml_path = os.path.join(os.path.dirname(__file__),'HAT_VietNews.yaml')
+yaml_path = os.path.join(os.path.dirname(__file__))
 with open(yaml_path, 'r', encoding='utf-8') as f:
     config = yaml.safe_load(f)
