d82cd0d7f46c6175fd2f1d9feeb1a6c760604d9a
hongngoc1210 <ngochnh121@gmail.com>
2025-12-04 23:06:55 +0700

    update init config that delete load yaml

--- configs/__init__.py
+++
@@ -1,7 +0,0 @@
-import yaml
-import os
-
-# Tự động load file YAML làm config mặc định
-yaml_path = os.path.join(os.path.dirname(__file__))
-with open(yaml_path, 'r', encoding='utf-8') as f:
-    config = yaml.safe_load(f)
